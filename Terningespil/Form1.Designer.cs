a760073aec4f0ba486e917aac951c8cc6f97b2af
iNukku <bertilandersen@hotmail.com>
2016-10-21 20:36:16 +0200

    First draft before classes

--- Terningespil/Form1.Designer.cs
+++ Terningespil/Form1.Designer.cs
@@ -28,12 +28,239 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dice_lbl_1 = new System.Windows.Forms.Label();
+            this.dice_lbl_2 = new System.Windows.Forms.Label();
+            this.dice_lbl_3 = new System.Windows.Forms.Label();
+            this.dice_lbl_4 = new System.Windows.Forms.Label();
+            this.dice_lbl_5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chosen_dice_lbl_1 = new System.Windows.Forms.Label();
+            this.chosen_dice_lbl_2 = new System.Windows.Forms.Label();
+            this.chosen_dice_lbl_3 = new System.Windows.Forms.Label();
+            this.chosen_dice_lbl_4 = new System.Windows.Forms.Label();
+            this.chosen_dice_lbl_5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // dice_lbl_1
+            // 
+            this.dice_lbl_1.Image = global::Terningespil.Properties.Resources.dice_blank;
+            this.dice_lbl_1.Location = new System.Drawing.Point(250, 220);
+            this.dice_lbl_1.Name = "dice_lbl_1";
+            this.dice_lbl_1.Size = new System.Drawing.Size(50, 50);
+            this.dice_lbl_1.TabIndex = 0;
+            // 
+            // dice_lbl_2
+            // 
+            this.dice_lbl_2.Image = global::Terningespil.Properties.Resources.dice_blank;
+            this.dice_lbl_2.Location = new System.Drawing.Point(310, 220);
+            this.dice_lbl_2.Name = "dice_lbl_2";
+            this.dice_lbl_2.Size = new System.Drawing.Size(50, 50);
+            this.dice_lbl_2.TabIndex = 1;
+            // 
+            // dice_lbl_3
+            // 
+            this.dice_lbl_3.Image = global::Terningespil.Properties.Resources.dice_blank;
+            this.dice_lbl_3.Location = new System.Drawing.Point(370, 220);
+            this.dice_lbl_3.Name = "dice_lbl_3";
+            this.dice_lbl_3.Size = new System.Drawing.Size(50, 50);
+            this.dice_lbl_3.TabIndex = 2;
+            // 
+            // dice_lbl_4
+            // 
+            this.dice_lbl_4.Image = global::Terningespil.Properties.Resources.dice_blank;
+            this.dice_lbl_4.Location = new System.Drawing.Point(430, 220);
+            this.dice_lbl_4.Name = "dice_lbl_4";
+            this.dice_lbl_4.Size = new System.Drawing.Size(50, 50);
+            this.dice_lbl_4.TabIndex = 3;
+            // 
+            // dice_lbl_5
+            // 
+            this.dice_lbl_5.Image = global::Terningespil.Properties.Resources.dice_blank;
+            this.dice_lbl_5.Location = new System.Drawing.Point(490, 220);
+            this.dice_lbl_5.Name = "dice_lbl_5";
+            this.dice_lbl_5.Size = new System.Drawing.Size(50, 50);
+            this.dice_lbl_5.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(315, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 86);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Kast terningerne";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 220);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Kast:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Gemt:";
+            // 
+            // chosen_dice_lbl_1
+            // 
+            this.chosen_dice_lbl_1.Image = global::Terningespil.Properties.Resources.dice_blank;
+            this.chosen_dice_lbl_1.Location = new System.Drawing.Point(250, 70);
+            this.chosen_dice_lbl_1.Name = "chosen_dice_lbl_1";
+            this.chosen_dice_lbl_1.Size = new System.Drawing.Size(50, 50);
+            this.chosen_dice_lbl_1.TabIndex = 8;
+            // 
+            // chosen_dice_lbl_2
+            // 
+            this.chosen_dice_lbl_2.Image = global::Terningespil.Properties.Resources.dice_blank;
+            this.chosen_dice_lbl_2.Location = new System.Drawing.Point(310, 70);
+            this.chosen_dice_lbl_2.Name = "chosen_dice_lbl_2";
+            this.chosen_dice_lbl_2.Size = new System.Drawing.Size(50, 50);
+            this.chosen_dice_lbl_2.TabIndex = 9;
+            // 
+            // chosen_dice_lbl_3
+            // 
+            this.chosen_dice_lbl_3.Image = global::Terningespil.Properties.Resources.dice_blank;
+            this.chosen_dice_lbl_3.Location = new System.Drawing.Point(370, 70);
+            this.chosen_dice_lbl_3.Name = "chosen_dice_lbl_3";
+            this.chosen_dice_lbl_3.Size = new System.Drawing.Size(50, 50);
+            this.chosen_dice_lbl_3.TabIndex = 10;
+            // 
+            // chosen_dice_lbl_4
+            // 
+            this.chosen_dice_lbl_4.Image = global::Terningespil.Properties.Resources.dice_blank;
+            this.chosen_dice_lbl_4.Location = new System.Drawing.Point(430, 70);
+            this.chosen_dice_lbl_4.Name = "chosen_dice_lbl_4";
+            this.chosen_dice_lbl_4.Size = new System.Drawing.Size(50, 50);
+            this.chosen_dice_lbl_4.TabIndex = 11;
+            // 
+            // chosen_dice_lbl_5
+            // 
+            this.chosen_dice_lbl_5.Image = global::Terningespil.Properties.Resources.dice_blank;
+            this.chosen_dice_lbl_5.Location = new System.Drawing.Point(490, 70);
+            this.chosen_dice_lbl_5.Name = "chosen_dice_lbl_5";
+            this.chosen_dice_lbl_5.Size = new System.Drawing.Size(50, 50);
+            this.chosen_dice_lbl_5.TabIndex = 12;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(250, 180);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(44, 24);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(310, 180);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(44, 24);
+            this.checkBox2.TabIndex = 14;
+            this.checkBox2.Text = "2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(370, 180);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(44, 24);
+            this.checkBox3.TabIndex = 15;
+            this.checkBox3.Text = "3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(430, 180);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(44, 24);
+            this.checkBox4.TabIndex = 16;
+            this.checkBox4.Text = "4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(490, 180);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(44, 24);
+            this.checkBox5.TabIndex = 17;
+            this.checkBox5.Text = "5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(778, 744);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.chosen_dice_lbl_5);
+            this.Controls.Add(this.chosen_dice_lbl_4);
+            this.Controls.Add(this.chosen_dice_lbl_3);
+            this.Controls.Add(this.chosen_dice_lbl_2);
+            this.Controls.Add(this.chosen_dice_lbl_1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dice_lbl_5);
+            this.Controls.Add(this.dice_lbl_4);
+            this.Controls.Add(this.dice_lbl_3);
+            this.Controls.Add(this.dice_lbl_2);
+            this.Controls.Add(this.dice_lbl_1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Terningespil";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label dice_lbl_1;
+        private System.Windows.Forms.Label dice_lbl_2;
+        private System.Windows.Forms.Label dice_lbl_3;
+        private System.Windows.Forms.Label dice_lbl_4;
+        private System.Windows.Forms.Label dice_lbl_5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label chosen_dice_lbl_1;
+        private System.Windows.Forms.Label chosen_dice_lbl_2;
+        private System.Windows.Forms.Label chosen_dice_lbl_3;
+        private System.Windows.Forms.Label chosen_dice_lbl_4;
+        private System.Windows.Forms.Label chosen_dice_lbl_5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
     }
 }
 
--- Terningespil/Form1.cs
+++ Terningespil/Form1.cs
@@ -8,13 +8,297 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Terningespil
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random();
+        bool checkBoxesAreHidden = true;
+        int[] thrownDices = new int[5];
+        int numberOfTries = 0;
+        //resultarray skal høre til den enkelte player-class, men den skal først laves!
+        int[] resultarray = new int[5];
+        bool[] diceIsChosen = new bool[] { false, false, false, false, false };
+
+        player playerOne = new player();
+        player playerTwo = new player();    
+
         public Form1()
         {
             InitializeComponent();
+            chosen_dice_lbl_1.Visible = false;
+            chosen_dice_lbl_2.Visible = false;
+            chosen_dice_lbl_3.Visible = false;
+            chosen_dice_lbl_4.Visible = false;
+            chosen_dice_lbl_5.Visible = false;
+            label2.Visible = false;
+
+            playerOne.name = "Player One";
+            playerTwo.name = "Player Two";
+
+            hideCheckboxes();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (numberOfTries < 2)
+            {
+                checkCheckboxMarks();
+                setSecondaryLabels();
+                rollDice();
+                setPrimaryDiceLabels();
+
+                if (checkBoxesAreHidden)
+                {
+                    showCheckboxes();
+                    label2.Visible = true;
+                }      
+                          
+                numberOfTries++;              
+            }
+            else
+            {
+                checkCheckboxMarks();
+                updateResultarray();
+                //Problem: Secondarylabels sættes ikke
+                setSecondaryLabels();
+                endRound();
+            }
+        }
+        /// <summary>
+        /// fylder thrownDices-array med tilfældige terningekast
+        /// </summary>
+        private void rollDice()
+        {
+            for (int i = 0; i < thrownDices.Length; i++)
+            {
+                if (diceIsChosen[i] == false)
+                {
+                    thrownDices[i] = rng.Next(1, 7);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+
+        /// <summary>
+        /// returnerer billede af terning svarende til den parsede værdi
+        /// </summary>
+        /// <param name="theRoll"></param>
+        /// <returns></returns>
+        /// 
+
+        private void setPrimaryDiceLabels()
+        {
+            dice_lbl_1.Image = findImage(thrownDices[0]);
+            dice_lbl_2.Image = findImage(thrownDices[1]);
+            dice_lbl_3.Image = findImage(thrownDices[2]);
+            dice_lbl_4.Image = findImage(thrownDices[3]);
+            dice_lbl_5.Image = findImage(thrownDices[4]);
+        }
+
+        private void setSecondaryLabels()
+        {
+            if (resultarray[0] != 0)
+            {
+                chosen_dice_lbl_1.Image = findImage(resultarray[0]);
+            }
+            if (resultarray[1] != 0)
+            {
+                chosen_dice_lbl_2.Image = findImage(resultarray[1]);
+            }
+            if (resultarray[2] != 0)
+            {
+                chosen_dice_lbl_3.Image = findImage(resultarray[2]);
+            }
+            if (resultarray[3] != 0)
+            {
+                chosen_dice_lbl_4.Image = findImage(resultarray[3]);
+            }
+            if (resultarray[4] != 0)
+            {
+                chosen_dice_lbl_5.Image = findImage(resultarray[4]);
+            }
+        }
+        
+        private Image findImage(int theRoll)
+        {
+            Image dicePic = Properties.Resources.dice_blank;
+
+            switch (theRoll)
+            {
+                case 1:
+                    dicePic = Properties.Resources.dice_1;
+                    break;
+                case 2:
+                    dicePic = Properties.Resources.dice_2;
+                    break;
+                case 3:
+                    dicePic = Properties.Resources.dice_3;
+                    break;
+                case 4:
+                    dicePic = Properties.Resources.dice_4;
+                    break;
+                case 5:
+                    dicePic = Properties.Resources.dice_5;
+                    break;
+                case 6:
+                    dicePic = Properties.Resources.dice_6;
+                    break;
+                default:
+                    dicePic = Properties.Resources.dice_blank;
+                    break;
+            }
+            return dicePic;
+        }
+
+        /// <summary>
+        /// Tjekker om alle checkbokse er synlige
+        /// </summary>
+        /// <returns></returns>
+        private bool checkboxIsVisible()
+        {
+            bool isTrue = false;
+            if (checkBox1.Visible && checkBox2.Visible && checkBox3.Visible && checkBox4.Visible && checkBox5.Visible)
+            {
+                isTrue = true;
+            }
+            return isTrue;
+        }
+        /// <summary>
+        /// sætter alle checkbokse til synlig
+        /// </summary>
+        private void showCheckboxes()
+        {
+            checkBox1.Visible = true;
+            checkBox2.Visible = true;
+            checkBox3.Visible = true;
+            checkBox4.Visible = true;
+            checkBox5.Visible = true;
+
+            checkBoxesAreHidden = false;
+        }
+        /// <summary>
+        /// Skjuler alle checkbokse
+        /// </summary>
+        private void hideCheckboxes()
+        {
+            checkBox1.Visible = false;
+            checkBox2.Visible = false;
+            checkBox3.Visible = false;
+            checkBox4.Visible = false;
+            checkBox5.Visible = false;
+
+            checkBoxesAreHidden = true;
+        }
+
+        /// <summary>
+        /// sætter antal forsøg til 0, skjuler tjekbokse og nulstiller terningerne i formen
+        /// </summary>
+        private void resetTable()
+        {
+            hideCheckboxes();
+            label2.Visible = false;
+            numberOfTries = 0;
+
+            dice_lbl_1.Image = Properties.Resources.dice_blank;
+            dice_lbl_2.Image = Properties.Resources.dice_blank;
+            dice_lbl_3.Image = Properties.Resources.dice_blank;
+            dice_lbl_4.Image = Properties.Resources.dice_blank;
+            dice_lbl_5.Image = Properties.Resources.dice_blank;
+
+            for (int i = 0; i < diceIsChosen.Length; i++)
+            {
+                diceIsChosen[i] = false;
+            }
+
+        }
+
+        private void updateResultarray()
+        {
+            for (int i = 0; i < resultarray.Length; i++)
+            {
+                if (resultarray[i] == 0)
+                {
+                    resultarray[i] = thrownDices[i];
+                }
+            }
+        }
+        /// <summary>
+        /// Checker hvilke terninger spilleren vælger at gemme, og gemmer dem i resultarray
+        /// </summary>
+        private void checkCheckboxMarks()
+        {
+            if (checkBox1.Checked)
+            {
+                resultarray[0] = thrownDices[0];
+                chosen_dice_lbl_1.Visible = true;
+                checkBox1.Visible = false;
+                dice_lbl_1.Visible = false;
+                diceIsChosen[0] = true;
+                checkBox1.Checked = false;
+            }
+            if (checkBox2.Checked)
+            {
+                resultarray[1] = thrownDices[1];
+                chosen_dice_lbl_2.Visible = true;
+                checkBox2.Visible = false;
+                dice_lbl_2.Visible = false;
+                diceIsChosen[1] = true;
+                checkBox2.Checked = false;
+            }
+            if (checkBox3.Checked)
+            {
+                resultarray[2] = thrownDices[2];
+                chosen_dice_lbl_3.Visible = true;
+                checkBox3.Visible = false;
+                dice_lbl_3.Visible = false;
+                diceIsChosen[2] = true;
+                checkBox3.Checked = false;
+            }
+            if (checkBox4.Checked)
+            {
+                resultarray[3] = thrownDices[3];
+                chosen_dice_lbl_4.Visible = true;
+                checkBox4.Visible = false;
+                dice_lbl_4.Visible = false;
+                diceIsChosen[3] = true;
+                checkBox4.Checked = false;
+            }
+            if (checkBox5.Checked)
+            {
+                resultarray[4] = thrownDices[4];
+                chosen_dice_lbl_5.Visible = true;
+                checkBox5.Visible = false;
+                dice_lbl_5.Visible = false;
+                diceIsChosen[4] = true;
+                checkBox5.Checked = false;
+            }
+        }
+
+        private void endRound()
+        {
+            button1.Enabled = false;
+            hideCheckboxes();
+            setSecondaryLabels();
+            MessageBox.Show("Your score was : " + resultarray.Sum().ToString());
+        }
+
+    }
+
+    public class player
+    {
+        public string name;
+        public int score = 0;
+        public bool hasTurn; 
+
+    }
+
+    public class Game
+    {
+
     }
 }
